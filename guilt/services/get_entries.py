f713c3a650b6febe3abf8f9bd380711a1156abc9
GeorgeRoe <george.roe@stfc.ac.uk>
2025-06-25 09:32:45 +0100

    feat: typed get entries service

--- guilt/services/get_entries.py
+++ guilt/services/get_entries.py
@@ -1,7 +1,8 @@
 import subprocess
+from pathlib import Path
 
 class GetEntriesPasswdResult:
-  def __init__(self, username, password, user_id, primary_group_id, info, home_directory, shell):
+  def __init__(self, username: str, password: str, user_id: str, primary_group_id: str, info: str, home_directory: Path, shell: Path) -> None:
     self.username = username
     self.password = password
     self.user_id = user_id
@@ -11,20 +12,25 @@ class GetEntriesPasswdResult:
     self.shell = shell
     
   @classmethod
-  def fromLine(cls, line):
+  def fromLine(cls, line: str) -> "GetEntriesPasswdResult":
     parts = line.strip().split(":")
     
     if len(parts) != 7:
       raise ValueError(f"Invalid line (expected 7 fields): {line}")
     
-    parts[2] = int(parts[2])
-    parts[3] = int(parts[3])
+    username = parts[0]
+    password = parts[1]
+    user_id = parts[2]
+    primary_group_id = parts[3]
+    info = parts[4]
+    home_directory = Path(parts[5])
+    shell = Path(parts[6])
     
-    return cls(*parts)
+    return cls(username, password, user_id, primary_group_id, info, home_directory, shell)
 
 class GetEntriesService:
   @classmethod
-  def passwd(cls):
+  def passwd(cls) -> list[GetEntriesPasswdResult]:
     command = ["getent", "passwd"]
       
     result = subprocess.run(command, capture_output=True, text=True)
