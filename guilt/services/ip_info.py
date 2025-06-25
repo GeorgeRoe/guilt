b67cb24b9f82f79b780ae610c5a5d88ef54e20b4
GeorgeRoe <george.roe@stfc.ac.uk>
2025-06-25 10:00:52 +0100

    feat: typed ip info service

--- guilt/services/ip_info.py
+++ guilt/services/ip_info.py
@@ -1,53 +1,58 @@
 import httpx
 import asyncio
+from guilt.utility.safe_get import safe_get_string
+from typing import Any
 
 class IpInfoResult:
-  def __init__(self, ip, hostname, city, region, country, loc, org, postal, timezone, readme):
+  def __init__(self, ip: str, hostname: str, city: str, region: str, country: str, latitude: float, longitude: float, organisation: str, postal: str, timezone: str):
     self.ip = ip
     self.hostname = hostname
     self.city = city
     self.region = region
     self.country = country
-    self.loc = loc
-    self.org = org
+    self.latitude = latitude
+    self.longitude = longitude
+    self.organisation = organisation
     self.postal = postal
     self.timezone = timezone
-    self.readme = readme
-    
-  @property
-  def latitude(self):
-    return float(self.loc.split(',')[0]) if self.loc else None
-
-  @property
-  def longitude(self):
-    return float(self.loc.split(',')[1]) if self.loc else None
   
   @classmethod
-  def fromDict(cls, data):
+  def fromDict(cls, data: dict[str, Any]):
+    ip = safe_get_string(data, "ip")
+    hostname = safe_get_string(data, "hostname")
+    city = safe_get_string(data, "city")
+    region = safe_get_string(data, "region")
+    country = safe_get_string(data, "country")
+    latitude, longitude = [float(value) for value in safe_get_string(data, "loc").split(",")]
+    organisation = safe_get_string(data, "org")
+    postal = safe_get_string(data, "postal")
+    timezone = safe_get_string(data, "timezone")
+    
     return cls(
-      data.get("ip"),
-      data.get("hostname"),
-      data.get("city"),
-      data.get("region"),
-      data.get("country"),
-      data.get("loc"),
-      data.get("org"),
-      data.get("postal"),
-      data.get("timezone"),
-      data.get("readme")
+      ip,
+      hostname,
+      city,
+      region,
+      country,
+      latitude,
+      longitude,
+      organisation,
+      postal,
+      timezone
     )
 
 class IpInfoService:
   @classmethod
-  def fetchData(cls):
+  def fetchData(cls) -> IpInfoResult:
     return IpInfoResult.fromDict(asyncio.run(cls.request()))
 
   @classmethod
-  async def request(cls):
+  async def request(cls) -> dict[str, Any]:
     async with httpx.AsyncClient() as client:
       response = await client.get("http://ipinfo.io")
 
       if response.status_code == 200:
+        print(response.json())
         return response.json()
       else:
         raise Exception(f"Error {response.status_code}: {response.text}")
