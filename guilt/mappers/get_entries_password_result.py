1f414d17414da27715b5f3263b4baeb72cf13163
GeorgeRoe <george.roe@stfc.ac.uk>
2025-07-01 10:25:07 +0100

    feat: move get entries password result construction into mapper

---
+++ guilt/mappers/get_entries_password_result.py
@@ -0,0 +1,20 @@
+from guilt.models.get_entires_password_result import GetEntriesPasswordResult
+from pathlib import Path
+
+class MapToGetEntriesPasswordResult:
+  @staticmethod
+  def from_command_line(line: str) -> GetEntriesPasswordResult:
+    parts = line.strip().split(":")
+    
+    if len(parts) != 7:
+      raise ValueError(f"Invalid line (expected 7 fields): {line}")
+    
+    return GetEntriesPasswordResult(
+      parts[0],
+      parts[1],
+      parts[2],
+      parts[3],
+      parts[4],
+      Path(parts[5]),
+      Path(parts[6])
+    )
--- guilt/services/get_entries.py
+++ guilt/services/get_entries.py
@@ -1,6 +1,6 @@
 import subprocess
-from pathlib import Path
 from guilt.models.get_entires_password_result import GetEntriesPasswordResult
+from guilt.mappers.get_entries_password_result import MapToGetEntriesPasswordResult
 
 class GetEntriesService: 
   @staticmethod
@@ -12,21 +12,4 @@ class GetEntriesService:
     if result.returncode != 0:
       raise Exception(f"Command failed with code {result.returncode}: {result.stderr.strip()}")
     
-    get_entries_password_results: list[GetEntriesPasswordResult] = []
-    for line in result.stdout.splitlines():
-      parts = line.strip().split(":")
-      
-      if len(parts) != 7:
-        raise ValueError(f"Invalid line (expected 7 fields): {line}")
-      
-      get_entries_password_results.append(GetEntriesPasswordResult(
-        parts[0],
-        parts[1],
-        parts[2],
-        parts[3],
-        parts[4],
-        Path(parts[5]),
-        Path(parts[6])
-      ))
-      
-    return get_entries_password_results
+    return [MapToGetEntriesPasswordResult.from_command_line(line) for line in result.stdout.splitlines()]
