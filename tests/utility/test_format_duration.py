5413735367d41b8feb1e4d73fe65c240da24fff2
GeorgeRoe <george.roe@stfc.ac.uk>
2025-07-07 20:21:20 +0100

    chore: use correct tab size

--- tests/utility/test_format_duration.py
+++ tests/utility/test_format_duration.py
@@ -2,18 +2,18 @@ import pytest
 from guilt.utility.format_duration import format_duration
 
 @pytest.mark.parametrize(
-    "seconds, expected",
-    [
-        (0, "0 seconds"),
-        (59, "59 seconds"),
-        (60, "1 minute"),
-        (61, "1 minute, 1 second"),
-        (3600, "1 hour"),
-        (3661, "1 hour, 1 minute, 1 second"),
-        (86400, "1 day"),
-        (90061, "1 day, 1 hour, 1 minute, 1 second"),
-        (172800, "2 days"),
-    ]
+	"seconds, expected",
+	[
+		(0, "0 seconds"),
+		(59, "59 seconds"),
+		(60, "1 minute"),
+		(61, "1 minute, 1 second"),
+		(3600, "1 hour"),
+		(3661, "1 hour, 1 minute, 1 second"),
+		(86400, "1 day"),
+		(90061, "1 day, 1 hour, 1 minute, 1 second"),
+		(172800, "2 days"),
+	]
 )
 def test_format_duration(seconds: float, expected: str) -> None:
-    assert format_duration(seconds) == expected
+	assert format_duration(seconds) == expected
--- tests/utility/test_format_grams.py
+++ tests/utility/test_format_grams.py
@@ -3,19 +3,19 @@ from typing import Union
 from guilt.utility.format_grams import format_grams
 
 @pytest.mark.parametrize(
-    "grams, expected",
-    [
-        (0.1234, "0.12 grams"),
-        (0.999, "1.00 grams"),
-        (1, "1 gram"),
-        (2, "2 grams"),
-        (999, "999 grams"),
-        (1000, "1.00 kilogram"),
-        (1500, "1.50 kilograms"),
-        (999999, "999.99 kilograms"),
-        (1_000_000, "1.00 tonne"),
-        (2_500_000, "2.50 tonnes"),
-    ]
+	"grams, expected",
+	[
+		(0.1234, "0.12 grams"),
+		(0.999, "1.00 grams"),
+		(1, "1 gram"),
+		(2, "2 grams"),
+		(999, "999 grams"),
+		(1000, "1.00 kilogram"),
+		(1500, "1.50 #kilograms"),
+		(999999, "999.99 kilograms"),
+		(1_000_000, "1.00 tonne"),
+		(2_500_000, "2.50 tonnes"),
+	]
 )
 def test_format_grams(grams: Union[int, float], expected: str):
-    assert format_grams(grams) == expected
+	assert format_grams(grams) == expected
