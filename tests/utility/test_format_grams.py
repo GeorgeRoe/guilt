a9f65188ef480acdfd08f6861cbb5001dbc44412
GeorgeRoe <george.roe@stfc.ac.uk>
2025-07-08 09:52:32 +0100

    chore: remove typo

--- tests/utility/test_format_grams.py
+++ tests/utility/test_format_grams.py
@@ -11,7 +11,7 @@ from guilt.utility.format_grams import format_grams
 		(2, "2 grams"),
 		(999, "999 grams"),
 		(1000, "1.00 kilogram"),
-		(1500, "1.50 #kilograms"),
+		(1500, "1.50 kilograms"),
 		(999999, "999.99 kilograms"),
 		(1_000_000, "1.00 tonne"),
 		(2_500_000, "2.50 tonnes"),
