1b511412c5188d3c79ef5f3ac17cdf2ca7ff6a3a
GeorgeRoe <george.roe@stfc.ac.uk>
2025-07-14 10:03:58 +0100

    feat: cpu profile test 100% coverage

--- tests/mappers/test_cpu_profile.py
+++ tests/mappers/test_cpu_profile.py
@@ -7,7 +7,7 @@ from tests.helpers.json_helpers import all_variants_with_one_key_removed, all_va
 VALID_JSON: dict[str, Json] = {
 	"name": "AMD EPYC 9654",
 	"tdp": 360,
-	"cores": 96	 
+	"cores": 96
 }
 
 def test_from_json_sucess() -> None:
@@ -17,15 +17,16 @@ def test_from_json_sucess() -> None:
 	assert result.name == "AMD EPYC 9654"
 	assert result.tdp == 360
 	assert result.cores == 96
+	assert result.tdp_per_core == 3.75
 
 @pytest.mark.parametrize(
-  "data",
-  [
-    (item)
-    for item
-    in all_variants_with_one_key_removed(VALID_JSON) + all_variants_with_one_key_incorrectly_typed(VALID_JSON, {"tdp"})
-  ]
+	"data",
+	[
+		(item)
+		for item
+		in all_variants_with_one_key_removed(VALID_JSON) + all_variants_with_one_key_incorrectly_typed(VALID_JSON, {"tdp"})
+	]
 )
 def test_from_json_invalid_raises(data: dict[str, Json]) -> None:
-  with pytest.raises(ValueError):
-    MapToCpuProfile.from_json(data)
+	with pytest.raises(ValueError):
+		MapToCpuProfile.from_json(data)
