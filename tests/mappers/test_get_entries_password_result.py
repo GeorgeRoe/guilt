48560b21e8c572cd71de0f5fd1ee3a2f99316a02
GeorgeRoe <george.roe@stfc.ac.uk>
2025-07-14 14:18:55 +0100

    feat: add tests for get entires password result mapper

---
+++ tests/mappers/test_get_entries_password_result.py
@@ -0,0 +1,18 @@
+from guilt.mappers.get_entries_password_result import MapToGetEntriesPasswordResult
+from guilt.models.get_entires_password_result import GetEntriesPasswordResult
+from pathlib import Path
+
+VALID_LINE = "username:password:user_id:primary_group_id:info:/path/to/home:/path/to/shell"
+
+def test_from_line_success() -> None:
+  result = MapToGetEntriesPasswordResult.from_line(VALID_LINE)
+
+  assert isinstance(result, GetEntriesPasswordResult)
+
+  assert result.username == "username"
+  assert result.password == "password"
+  assert result.user_id == "user_id"
+  assert result.primary_group_id == "primary_group_id"
+  assert result.info == "info"
+  assert result.home_directory == Path("/path/to/home")
+  assert result.shell == Path("/path/to/shell")
