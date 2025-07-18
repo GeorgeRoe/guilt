1a0709ce3d6787d2f33a395b7dd1cac0742bf8ba
GeorgeRoe <george.roe@stfc.ac.uk>
2025-07-18 22:25:07 +0100

    feat: add tests for guilt directory service

---
+++ tests/mocks/services/environment_variables.py
@@ -0,0 +1,28 @@
+from guilt.interfaces.services.environment_variables import EnvironmentVariablesServiceInterface
+from typing import Union
+from pathlib import Path
+
+class MockEnvironmentVariablesService(EnvironmentVariablesServiceInterface):
+  def __init__(self, variables: dict[str, str], home_dir: Path) -> None:
+    self._variables = variables
+    self._home_dir = home_dir
+    
+  def get_variable(self, name: str) -> Union[str, None]:
+    return self._variables.get(name)
+    
+  def ensure_get_variable(self, name: str) -> str:
+    variable = self.get_variable(name)
+    
+    if variable is None:
+      raise ValueError(f"The environment variable '{name}' must be set.")
+    
+    return variable
+  
+  def get_user(self) -> str:
+    return self.ensure_get_variable("USER")
+  
+  def get_home_directory(self) -> Path:
+    home = self.get_variable("HOME")
+    if home:
+      return Path(home).expanduser().resolve()
+    return self._home_dir
