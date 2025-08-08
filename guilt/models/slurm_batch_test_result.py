3eeaa3ae557884ccec040e8270d20a60ad1996f7
GeorgeRoe <george.roe@stfc.ac.uk>
2025-08-08 16:41:31 +0100

    chore: remove dataclass from SlurmBatchTestResult class

--- guilt/models/slurm_batch_test_result.py
+++ guilt/models/slurm_batch_test_result.py
@@ -1,10 +1,36 @@
-from dataclasses import dataclass
 from datetime import datetime
 
-@dataclass
-class SlurmBatchTestResult:
-  job_id: str
-  start_time: datetime
-  processor_count: int
-  nodes: str
-  partition: str
+class SlurmBatchTestResultInterface:
+  def __init__(
+    self,
+    job_id: str,
+    start_time: datetime,
+    processor_count: int,
+    nodes: str,
+    partition: str
+  ) -> None:
+    self._job_id = job_id
+    self._start_time = start_time
+    self._processor_count = processor_count
+    self._nodes = nodes
+    self._partition = partition
+
+  @property
+  def job_id(self) -> str:
+    return self._job_id
+  
+  @property
+  def start_time(self) -> datetime:
+    return self._start_time
+  
+  @property
+  def processor_count(self) -> int:
+    return self._processor_count
+  
+  @property
+  def nodes(self) -> str:
+    return self._nodes
+  
+  @property
+  def partition(self) -> str:
+    return self._partition
