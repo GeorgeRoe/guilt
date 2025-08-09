124a9cdc56959fd6243e7c4a4b0ae6151b6665fe
GeorgeRoe <george.roe@stfc.ac.uk>
2025-08-09 15:18:46 +0100

    feat: setup command creates guilt directory

--- src/commands/setup.rs
+++ src/commands/setup.rs
@@ -1,4 +1,15 @@
+use std::env;
+use std::fs;
+use std::io;
+
 pub fn run() -> Result<(), Box<dyn std::error::Error>> {
-    println!("NOT IMPLEMENTED");
+    let mut home = env::home_dir().ok_or_else(|| io::Error::new(io::ErrorKind::NotFound, "Could not find home directory"))?;
+
+    home.push(".guilt");
+
+    fs::create_dir_all(&home)?;
+
+    println!("Created '.guilt' directory at {}", home.display());
+
     Ok(())
 }
