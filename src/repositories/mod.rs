76b02ebbf9797a72e2b96720f4d9dfa17c0aae31
GeorgeRoe <george.roe@stfc.ac.uk>
2025-08-18 16:52:52 +0100

    chore: add processed jobs repository error class for more detailed errors

--- src/repositories/mod.rs
+++ src/repositories/mod.rs
@@ -1,11 +1,11 @@
 mod cpu_profiles;
 pub use cpu_profiles::{CpuProfilesRepository, CpuProfilesRepositoryError};
 
+mod processed_jobs;
+pub use processed_jobs::{ProcessedJobsRepository, ProcessedJobsRepositoryError};
+
 mod unprocessed_jobs;
 pub use unprocessed_jobs::UnprocessedJobsRepository;
-
-mod processed_jobs;
-pub use processed_jobs::ProcessedJobsRepository;
 
 mod user_data;
 pub use user_data::UserDataRepository;
