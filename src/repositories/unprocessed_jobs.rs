454fbd09793bf60659b9e1f5874d62a3a7262c4f
GeorgeRoe <george.roe@stfc.ac.uk>
2025-08-18 19:16:12 +0100

    chore: add unprocessed jobs repository error enum for more detailed errors

--- src/repositories/unprocessed_jobs.rs
+++ src/repositories/unprocessed_jobs.rs
@@ -1,10 +1,16 @@
-use crate::SomeError;
 use crate::models::UnprocessedJob;
 use std::result::Result;
+use thiserror::Error;
+
+#[derive(Error, Debug)]
+pub enum UnprocessedJobsRepositoryError {
+    #[error("Referenced CPU profile not found: {0}")]
+    MissingCpuProfile(String)
+}
 
 pub trait UnprocessedJobsRepository {
-    fn get_all_unprocessed_jobs(&self) -> Result<Vec<UnprocessedJob>, SomeError>;
-    fn get_unprocessed_job_by_id(&self, id: &str) -> Result<Option<UnprocessedJob>, SomeError>;
-    fn upsert_unprocessed_job(&mut self, job: &UnprocessedJob) -> Result<(), SomeError>;
-    fn delete_unprocessed_job(&mut self, id: &str) -> Result<(), SomeError>;
+    fn get_all_unprocessed_jobs(&self) -> Result<Vec<UnprocessedJob>, UnprocessedJobsRepositoryError>;
+    fn get_unprocessed_job_by_id(&self, id: &str) -> Result<Option<UnprocessedJob>, UnprocessedJobsRepositoryError>;
+    fn upsert_unprocessed_job(&mut self, job: &UnprocessedJob) -> Result<(), UnprocessedJobsRepositoryError>;
+    fn delete_unprocessed_job(&mut self, id: &str) -> Result<(), UnprocessedJobsRepositoryError>;
 }
