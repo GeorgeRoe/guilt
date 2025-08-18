25b18a99a374bfe4f8193d1ebd014a7fd9efc440
GeorgeRoe <george.roe@stfc.ac.uk>
2025-08-18 16:47:31 +0100

    chore: add empty cpu profiles repository error for future

--- src/repositories/cpu_profiles.rs
+++ src/repositories/cpu_profiles.rs
@@ -1,10 +1,13 @@
-use crate::SomeError;
 use crate::models::CpuProfile;
 use std::result::Result;
+use thiserror::Error;
+
+#[derive(Error, Debug)]
+pub enum CpuProfilesRepositoryError {}
 
 pub trait CpuProfilesRepository {
-    fn get_all_cpu_profiles(&self) -> Result<Vec<CpuProfile>, SomeError>;
-    fn get_cpu_profile_by_name(&self, name: &str) -> Result<Option<CpuProfile>, SomeError>;
-    fn upsert_cpu_profile(&mut self, profile: &CpuProfile) -> Result<(), SomeError>;
-    fn delete_cpu_profile(&mut self, name: &str) -> Result<(), SomeError>;
+    fn get_all_cpu_profiles(&self) -> Result<Vec<CpuProfile>, CpuProfilesRepositoryError>;
+    fn get_cpu_profile_by_name(&self, name: &str) -> Result<Option<CpuProfile>, CpuProfilesRepositoryError>;
+    fn upsert_cpu_profile(&mut self, profile: &CpuProfile) -> Result<(), CpuProfilesRepositoryError>;
+    fn delete_cpu_profile(&mut self, name: &str) -> Result<(), CpuProfilesRepositoryError>;
 }
