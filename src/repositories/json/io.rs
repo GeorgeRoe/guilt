42fe25d1f4419d8fb08033ea29a5b73255f81ec0
GeorgeRoe <george.roe@stfc.ac.uk>
2025-08-19 09:21:48 +0100

    chore: format

--- src/repositories/json/io.rs
+++ src/repositories/json/io.rs
@@ -1,8 +1,8 @@
 use serde::{Serialize, de::DeserializeOwned};
 use std::fs::File;
+use std::io;
 use std::io::{BufReader, BufWriter};
 use std::path::Path;
-use std::io;
 use thiserror::Error;
 
 #[derive(Error, Debug)]
@@ -14,14 +14,19 @@ pub enum JsonFileOperationError {
     Serde(#[from] serde_json::Error),
 }
 
-pub fn read_json_file<T: DeserializeOwned, P: AsRef<Path>>(path: P) -> Result<T, JsonFileOperationError> {
+pub fn read_json_file<T: DeserializeOwned, P: AsRef<Path>>(
+    path: P,
+) -> Result<T, JsonFileOperationError> {
     let file = File::open(path).map_err(JsonFileOperationError::Io)?;
     let reader = BufReader::new(file);
     let data = serde_json::from_reader(reader).map_err(JsonFileOperationError::Serde)?;
     Ok(data)
 }
 
-pub fn write_json_file<T: Serialize, P: AsRef<Path>>(path: P, data: &T) -> Result<(), JsonFileOperationError> {
+pub fn write_json_file<T: Serialize, P: AsRef<Path>>(
+    path: P,
+    data: &T,
+) -> Result<(), JsonFileOperationError> {
     let file = File::create(path).map_err(JsonFileOperationError::Io)?;
     let writer = BufWriter::new(file);
     serde_json::to_writer_pretty(writer, data).map_err(JsonFileOperationError::Serde)?;
