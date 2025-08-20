29a7a2ec30c7fa48cd39ca82e83e8d18cf096dab
GeorgeRoe <george.roe@stfc.ac.uk>
2025-08-20 14:14:43 +0100

    feat: add simple forecast summary

--- src/commands/forecast.rs
+++ src/commands/forecast.rs
@@ -1,6 +1,42 @@
 use crate::SomeError;
+use crate::carbon_intensity_api::CarbonIntensityAggregator;
+use crate::ip_info::fetch_ip_info;
+use chrono::{DateTime, Duration, Local, Utc};
 
-pub fn run() -> Result<(), SomeError> {
-    println!("NOT IMPLEMENTED");
+static DATE_TIME_FORMAT: &str = "%Y-%m-%d %H:%M %:z";
+
+fn format_utc_datetime(dt: DateTime<Utc>) -> String {
+    dt.with_timezone(&Local)
+        .format(DATE_TIME_FORMAT)
+        .to_string()
+}
+
+pub async fn run() -> Result<(), SomeError> {
+    let ip_info = fetch_ip_info().await?;
+    let mut aggregator = CarbonIntensityAggregator::new(ip_info.postal);
+
+    let duration = Duration::days(1);
+    let now: DateTime<Utc> = Utc::now();
+    let finish = now + duration;
+
+    let forecast = aggregator.get_segments(now, finish).await?;
+    if forecast.is_empty() {
+        println!("No forecast data available for the next {} day(s).", duration.num_days());
+        return Ok(());
+    }
+
+    println!("Forecast Summary over next {} day(s):", duration.num_days());
+
+    if let Some(min_intensity) = forecast.iter().map(|s| s.intensity).min() {
+        println!("  Minimum Intensity: {} gCO2eq/kWh", min_intensity);
+    }
+
+    if let Some(max_intensity) = forecast.iter().map(|s| s.intensity).max() {
+        println!("  Maximum Intensity: {} gCO2eq/kWh", max_intensity);
+    }
+
+    let average_intensity = aggregator.get_average_intensity(now, finish).await?;
+    println!("  Average Intensity: {:.3} gCO2eq/kWh", average_intensity);
+
     Ok(())
 }
