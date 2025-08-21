af13d86e88086bdcc6106fa7d3b45bb16ffecba7
GeorgeRoe <george.roe@stfc.ac.uk>
2025-08-21 22:40:55 +0100

    feat: add basic report command

--- src/commands/report.rs
+++ src/commands/report.rs
@@ -1,6 +1,22 @@
 use crate::SomeError;
+use crate::repositories::json::JsonUserDataRepository;
+use crate::repositories::{UserDataRepository, ProcessedJobsRepository};
+use crate::users::get_current_user;
+use colored::Colorize;
 
 pub fn run() -> Result<(), SomeError> {
-    println!("NOT IMPLEMENTED");
+    let current_user = get_current_user()?;
+
+    let user_data_repo = JsonUserDataRepository::new(current_user)?;
+
+    let processed_jobs = user_data_repo.get_all_processed_jobs()?;
+
+    if processed_jobs.is_empty() {
+        println!("{}", "No processed jobs found.".red());
+    } else {
+        let total_emissions = processed_jobs.iter().map(|job| job.emissions).sum::<f64>();
+        println!("You have emitted {:.3} grams of CO2.", total_emissions);
+    }
+
     Ok(())
 }
