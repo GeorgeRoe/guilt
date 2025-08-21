a4f5021b72cd61af52aa5908e4b278454377fa97
GeorgeRoe <george.roe@stfc.ac.uk>
2025-08-21 11:45:49 +0100

    feat: remove horribly messy and low level svg rendering code

--- src/plotting/kitty_plotter.rs
+++ src/plotting/kitty_plotter.rs
@@ -3,11 +3,8 @@ use crate::SomeError;
 use std::process::Command;
 use std::fs::remove_file;
 use thiserror::Error;
-use charts_rs::{BarChart, Series};
-use usvg::{fontdb::Database, Options, Tree};
-use tiny_skia::{Pixmap, Transform};
-use resvg::render;
-use std::sync::Arc;
+use charts_rs::{BarChart, Series, svg_to_png, get_or_try_init_fonts};
+use std::fs;
 
 #[derive(Debug, Error)]
 pub enum KittyPlottingError {
@@ -42,21 +39,9 @@ impl KittyPlotter {
     }
 
     fn display_svg(&self, svg_data: &str) -> Result<(), SomeError> {
-        let mut opt = Options::default();
-        let mut db = Database::new();
-        db.load_system_fonts();
-        opt.fontdb = Arc::new(db);
-
-        let tree = Tree::from_str(svg_data, &opt)?;
-
-        let pixmap_size = tree.size().to_int_size();
-        let mut pixmap = Pixmap::new(pixmap_size.width(), pixmap_size.height())
-            .ok_or("Failed to create pixmap")?;
-
-        render(&tree, Transform::default(), &mut pixmap.as_mut());
-
-        pixmap.save_png("temp.svg")?;
-        self.display_png("temp.svg")?;
+        let png_data = svg_to_png(svg_data)?;
+        fs::write("temp.png", &png_data)?;
+        self.display_png("temp.png")?;
 
         Ok(())
     }
@@ -64,6 +49,9 @@ impl KittyPlotter {
 
 impl Plotter for KittyPlotter {
     fn draw_generation_mix(&self, generation_mix: std::collections::HashMap<String, f64>) -> Result<(), SomeError> {
+        let data = include_bytes!("../../assets/Roboto.ttf") as &[u8];
+        get_or_try_init_fonts(Some(vec![data])).unwrap();
+
         let series = Series::new("Generation Mix".to_string(), generation_mix.values().cloned().map(|v| v as f32).collect());
         let labels: Vec<String> = generation_mix.keys().cloned().collect();
 
@@ -73,7 +61,7 @@ impl Plotter for KittyPlotter {
         bar_chart.title_text = "Generation Mix".to_string();
         bar_chart.legend_show = Some(false);
         // TODO: allow user to configure this setting later
-        bar_chart.font_family = "DejaVu Sans".to_string();
+        bar_chart.font_family = "Roboto".to_string();
 
         self.display_svg(&bar_chart.svg()?)?;
 
