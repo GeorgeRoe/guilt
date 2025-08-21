887d50d0edb6fb165d60f159ed4758c8ec6be1de
GeorgeRoe <george.roe@stfc.ac.uk>
2025-08-21 11:47:33 +0100

    fix: load roboto font once at kitty plotter initialisation

--- src/plotting/mod.rs
+++ src/plotting/mod.rs
@@ -9,7 +9,7 @@ pub use terminal_plotter::TerminalPlotter;
 
 pub fn get_plotter() -> Box<dyn Plotter> {
     if std::env::var("TERM") == Ok("xterm-kitty".to_string()) {
-        Box::new(KittyPlotter)
+        Box::new(KittyPlotter::new())
     } else {
         Box::new(TerminalPlotter)
     }
