5ff1eafdcbab79841403959cf2f3e6505d1f1d13
GeorgeRoe <george.roe@stfc.ac.uk>
2025-08-21 11:24:33 +0100

    feat: move to charts rs for plotting as plotters is unnecessarily complex

--- src/plotting/kitty_plotter.rs
+++ src/plotting/kitty_plotter.rs
@@ -1,15 +1,13 @@
 use super::Plotter;
-use plotters::prelude::*;
 use crate::SomeError;
 use std::process::Command;
 use std::fs::remove_file;
 use thiserror::Error;
-
-static TEMPORARY_FILE: &str = "plot.png";
-
-fn is_source_renewable(source: &str) -> bool {
-    matches!(source, "solar" | "wind" | "hydro" | "biomass")
-}
+use charts_rs::{BarChart, Series};
+use usvg::{fontdb::Database, Options, Tree};
+use tiny_skia::{Pixmap, Transform};
+use resvg::render;
+use std::sync::Arc;
 
 #[derive(Debug, Error)]
 pub enum KittyPlottingError {
@@ -23,12 +21,12 @@ pub enum KittyPlottingError {
 pub struct KittyPlotter;
 
 impl KittyPlotter {
-    fn display(&self) -> Result<(), KittyPlottingError> {
+    fn display_png(&self, file_name: &str) -> Result<(), KittyPlottingError> {
         let status = Command::new("kitty")
-            .args(&["+kitten", "icat", TEMPORARY_FILE])
+            .args(&["+kitten", "icat", file_name])
             .status();
 
-        remove_file(TEMPORARY_FILE)?;
+        remove_file(file_name)?;
         
         match status {
             Ok(status) => {
@@ -42,48 +40,42 @@ impl KittyPlotter {
             Err(e) => Err(KittyPlottingError::Display(e.to_string()))
         }
     }
+
+    fn display_svg(&self, svg_data: &str) -> Result<(), SomeError> {
+        let mut opt = Options::default();
+        let mut db = Database::new();
+        db.load_system_fonts();
+        opt.fontdb = Arc::new(db);
+
+        let tree = Tree::from_str(svg_data, &opt)?;
+
+        let pixmap_size = tree.size().to_int_size();
+        let mut pixmap = Pixmap::new(pixmap_size.width(), pixmap_size.height())
+            .ok_or("Failed to create pixmap")?;
+
+        render(&tree, Transform::default(), &mut pixmap.as_mut());
+
+        pixmap.save_png("temp.svg")?;
+        self.display_png("temp.svg")?;
+
+        Ok(())
+    }
 }
 
 impl Plotter for KittyPlotter {
     fn draw_generation_mix(&self, generation_mix: std::collections::HashMap<String, f64>) -> Result<(), SomeError> {
-        let root = BitMapBackend::new(TEMPORARY_FILE, (800, 600)).into_drawing_area();
-        root.fill(&WHITE)?;
-        
-        let largest_percentage = generation_mix.values().cloned().fold(0.0, f64::max);
+        let series = Series::new("Generation Mix".to_string(), generation_mix.values().cloned().map(|v| v as f32).collect());
+        let labels: Vec<String> = generation_mix.keys().cloned().collect();
 
-        let mut chart = ChartBuilder::on(&root)
-            .caption("Generation Mix", ("sans-serif", 20))
-            .margin(10)
-            .x_label_area_size(40)
-            .y_label_area_size(50)
-            .build_cartesian_2d(0..generation_mix.len(), 0f64..(largest_percentage + 5.0))?;
+        let mut bar_chart = BarChart::new(vec![series], labels);
 
-        chart.configure_mesh()
-            .x_labels(generation_mix.len())
-            .x_label_formatter(&|x| {
-                let i = *x as usize;
-                generation_mix.keys().nth(i).unwrap_or(&"".to_string()).clone()
-            })
-            .x_desc("Source")
-            .y_desc("Percentage")
-            .draw()?;
+        bar_chart.y_axis_configs[0].axis_width = Some(55.0);
+        bar_chart.title_text = "Generation Mix".to_string();
+        bar_chart.legend_show = Some(false);
+        // TODO: allow user to configure this setting later
+        bar_chart.font_family = "DejaVu Sans".to_string();
 
-        chart.draw_series(
-            generation_mix.into_iter().enumerate().map(|(i, (source, percentage))| {
-                Rectangle::new(
-                    [(i, 0.0), ((i + 1), percentage)],
-                    if is_source_renewable(&source) {
-                        GREEN.filled()
-                    } else {
-                        RED.filled()
-                    }
-                )
-            }),
-        )?;
-
-        root.present()?;
-
-        self.display()?;
+        self.display_svg(&bar_chart.svg()?)?;
 
         Ok(())
     }
