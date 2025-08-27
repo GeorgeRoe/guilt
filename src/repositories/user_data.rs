3b95208ab12cb0d4299ff51536061940cda89791
GeorgeRoe <george.roe@stfc.ac.uk>
2025-08-27 19:39:05 +0100

    feat: make repositories take a reference to a user

--- src/repositories/user_data.rs
+++ src/repositories/user_data.rs
@@ -6,8 +6,8 @@ use std::result::Result;
 pub trait UserDataRepository:
     CpuProfilesRepository + UnprocessedJobsRepository + ProcessedJobsRepository
 {
-    fn setup(user: User) -> Result<(), SomeError>;
-    fn new(user: User) -> Result<Self, SomeError>
+    fn setup(user: &User) -> Result<(), SomeError>;
+    fn new(user: &User) -> Result<Self, SomeError>
     where
         Self: Sized;
     fn commit(&self) -> Result<(), SomeError>;
