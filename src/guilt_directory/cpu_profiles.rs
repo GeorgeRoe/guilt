18d797bd1e7093b7cc82754b0a9616de5ef5dc1f
GeorgeRoe <george.roe@stfc.ac.uk>
2025-09-30 15:30:45 +0100

    feat: add cpu profiles collection

---
+++ src/guilt_directory/cpu_profiles.rs
@@ -0,0 +1,37 @@
+use std::collections::HashMap;
+use std::path::Path;
+use crate::models::CpuProfile;
+use crate::json_io::*;
+
+pub struct CpuProfiles {
+    cache: HashMap<String, CpuProfile>,
+}
+
+impl CpuProfiles {
+    pub fn read(path: &Path) -> Result<Self, JsonFileOperationError> {
+        let profiles: Vec<CpuProfile> = read_json_file(path)?;
+        let cache = profiles.into_iter().map(|p| (p.name.clone(), p)).collect();
+        Ok(Self{ cache })
+    }
+
+    pub fn write(&self, path: &Path) -> Result<(), JsonFileOperationError> {
+        let profiles: Vec<&CpuProfile> = self.cache.values().collect();
+        write_json_file(path, &profiles)
+    }
+
+    pub fn get(&self, name: &str) -> Option<CpuProfile> {
+        if let Some(cpu_profile) = self.cache.get(name) {
+            Some(cpu_profile.clone())
+        } else {
+            None
+        }
+    }
+
+    pub fn upsert(&mut self, profile: CpuProfile) {
+        self.cache.insert(profile.name.clone(), profile);
+    }
+
+    pub fn remove(&mut self, name: &str) {
+        self.cache.remove(name);
+    }
+}
