70f8a4a9ad99c16014d603be5875767b4fc53376
GeorgeRoe <george.roe@stfc.ac.uk>
2025-09-30 22:13:17 +0100

    feat: add guilt directory manager

---
+++ src/guilt_directory/paths.rs
@@ -0,0 +1,3 @@
+pub const CPU_PROFILES_FILE: &str = "cpu_profiles.json";
+pub const UNPROCESSED_JOBS_FILE: &str = "unprocessed_jobs.json";
+pub const PROCESSED_JOBS_FILE: &str = "processed_jobs.json";
