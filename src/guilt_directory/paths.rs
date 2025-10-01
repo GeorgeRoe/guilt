3424e6252c79f91d3f0783fa6cadbf297b61bf32
GeorgeRoe <george.roe@stfc.ac.uk>
2025-10-01 08:17:25 +0100

    feat: add last written version to directory manager

--- src/guilt_directory/paths.rs
+++ src/guilt_directory/paths.rs
@@ -1,3 +1,4 @@
 pub const CPU_PROFILES_FILE: &str = "cpu_profiles.json";
 pub const UNPROCESSED_JOBS_FILE: &str = "unprocessed_jobs.json";
 pub const PROCESSED_JOBS_FILE: &str = "processed_jobs.json";
+pub const LAST_WRITTEN_VERSION_FILE: &str = "last_written_version";
