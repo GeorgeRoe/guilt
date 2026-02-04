8e77e59b895271f49b3a16ae8b11edd234adf742
GeorgeRoe <george.roe@stfc.ac.uk>
2026-02-04 09:51:36 +0000

    feat: update unprocessed job to have new resolve cpu profile method with none

--- src/guilt_directory/paths.rs
+++ src/guilt_directory/paths.rs
@@ -5,6 +5,7 @@ pub const CPU_PROFILES_FILE: &str = "cpu_profiles.json";
 pub const UNPROCESSED_JOBS_FILE: &str = "unprocessed_jobs.json";
 pub const PROCESSED_JOBS_FILE: &str = "processed_jobs.json";
 pub const LAST_WRITTEN_VERSION_FILE: &str = "last_written_version";
+pub const PROFILE_RESOLUTION_POLICY_FILE: &str = "profile_resolution_policy.rhai";
 
 pub const GUILT_DIR: &str = ".guilt";
 
